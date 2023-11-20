feaeafa428fa6efd5b74d94b28343b82d7bf1a55
DarkReaper231 <blackcreepercz012345@gmail.com>
2023-11-21 00:42:22 +0100

    Backported V9

--- README.md
+++ README.md
@@ -5,6 +5,14 @@ You can run it on apache, nginx or any other web server.
 The Android version has same assets as PC version.  
 All the differences are made with js/main.min.js and js/index.min.js
 
+# Note
+Version 9 came out, keep in mind that this is an old build of Incredibox
+from times when Incredibox wasn't obfuscated yet (0.5.4), you can
+play V9 just fine since I backported it to 0.5.4 in this project, mixes
+work just fine out of the box as the server only cares about version
+
+I am not obligated to fix any bugs that might arrise from backporting!
+
 # Branches
 There are 2 branches `android` and `desktop`  
 The desktop branch contains the version you can get on Steam or Mac  
---
+++ asset-v9/app.js
@@ -0,0 +1,94 @@
+/* 0.5.4 2021-04-23 21:06:13 */
+var app = new function() {
+    this.name = "Wekiddy", this.version = "9", this.date = "2023", this.folder = "asset-v9/", this.looptime = 5053, this.bpm = 190, this.totalframe = 242, this.nbpolo = 7, this.nbloopbonus = 2, this.bonusloopA = !0, this.bonusendloopA = !1, this.recmaxloop = 27, this.recminloop = 2, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#00102b", this.col0 = "#008eff", this.col1 = "#005cfa", this.col2 = "#0048c3", this.col3 = "#00348c", this.col4 = "#002055", this.animearray = [{
+        name: "01_boo_9",
+        color: "9448ff",
+        uniqsnd: !1
+    }, {
+        name: "02_kevin",
+        color: "fa340f",
+        uniqsnd: !1
+    }, {
+        name: "03_double_k",
+        color: "19d428",
+        uniqsnd: !1
+    }, {
+        name: "04_blue_gt",
+        color: "008eff",
+        uniqsnd: !0
+    }, {
+        name: "05_mj_182",
+        color: "19d428",
+        uniqsnd: !0
+    }, {
+        name: "06_boom_fuzz",
+        color: "#2c86fe",
+        uniqsnd: !1
+    }, {
+        name: "07_asap_bee",
+        color: "19d428",
+        uniqsnd: !1
+    }, {
+        name: "08_m_o_g",
+        color: "#ffca06",
+        uniqsnd: !1
+    }, {
+        name: "09_arashi",
+        color: "008eff",
+        uniqsnd: !1
+    }, {
+        name: "10_big_duke",
+        color: "fa340f",
+        uniqsnd: !1
+    }, {
+        name: "11_swingy",
+        color: "2c86fe",
+        uniqsnd: !1
+    }, {
+        name: "12_d_invaders",
+        color: "19d428",
+        uniqsnd: !1
+    }, {
+        name: "13_doo_doo",
+        color: "008eff",
+        uniqsnd: !0
+    }, {
+        name: "14_8_bit",
+        color: "19d428",
+        uniqsnd: !1
+    }, {
+        name: "15_kc_glow",
+        color: "9448ff",
+        uniqsnd: !1
+    }, {
+        name: "16_el_cool_p",
+        color: "fa340f",
+        uniqsnd: !1
+    }, {
+        name: "17_memphis",
+        color: "2c86fe",
+        uniqsnd: !1
+    }, {
+        name: "18_jogg_d",
+        color: "ffca06",
+        uniqsnd: !1
+    }, {
+        name: "19_scooter",
+        color: "19d428",
+        uniqsnd: !1
+    }, {
+        name: "20_lil_blaze",
+        color: "2c86fe",
+        uniqsnd: !1
+    }], this.bonusarray = [{
+        name: "Flow",
+        src: "v9-b1-flow-hb.mp4",
+        code: "1,2,5,8,17",
+        sound: "21_bonus_flow",
+        aspire: "aspire_flow"
+    }];
+    for (var e = 0, n = this.animearray.length; e < n; e++) {
+        var o = this.animearray[e].name;
+        this.animearray[e].soundA = o + "_a", this.animearray[e].soundB = this.animearray[e].uniqsnd ? o + "_a" : o + "_b", this.animearray[e].anime = o + "-sprite.png", this.animearray[e].animeData = o + ".json"
+    }
+};
